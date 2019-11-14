31c9585ad801f215d36ece3487047190c8def786
njmsaikat <njmsaikat@gmail.com>
2019-11-14 16:00:20 +0600

    portfolio sommit

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Saikat Roy</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="header.css">
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<header>
+		<div class="nav-head">
+			<div class="measure">
+				<div class="head-name">
+					<h1>Saikat Royy</h1>
+				</div>
+				<div class="head-cont">
+					<ul>
+					<p>Front-end Developer</p>
+
+					<a href="">Contact Me</a>
+					</ul>
+				</div>
+			</div>
+		</div> <!-- end of header -->
+	</header>
+
+	<div class="sidebar">
+		<?php include"sidebar/sidebar.php" ?>
+	</div>
+
+	<div class="main-body">
+		<div class="inner-body measure">
+			<div class="first-page" id="profile">
+
+						<!-- <p>faafafafaf</p> -->
+					<div class="image-profile measure">
+						<img src="face.jpg" alt="Saikat Roy">
+					</div>
+					<div class="prof-name">
+						<h2>Saikat Roy</h2>
+					</div>
+
+					<div class="social">
+						<a class="btn" href="#">
+							<i class="fa fa-facebook"></i>			
+						</a>
+						<a class="btn" href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a class="btn" href="#">
+							<i class="fa fa-google"></i>
+						</a>
+						<a class="btn" href="#">
+							<i class="fa fa-instagram"></i>
+						</a>
+						<a class="btn" href="#">
+							<i class="fa fa-youtube"></i>
+						</a>
+					</div>
+
+				<!-- start about -->
+
+				<div class="about" id="about">
+					<h2>About Me</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna <br> aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit <br> in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim <br> id est laborum. lorem</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna <br> aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit id est laborum. lorem</p>
+				</div>
+				<!-- end about -->
+
+			</div>
+
+				<!-- experience start -->
+			
+			<div class="experience" id="experience">
+					<h2>Experiences of Working</h2>
+					
+					<div class="left-experience">
+						<span class="experience-boxing">
+							<p>Web Development Fornt-end</p>
+						</span>
+						<span>
+							<p>Content Writing</p>
+						</span>
+						<span>
+							<p>Problem Solving</p>
+						</span>	
+					</div>
+
+					<div class="right-experience">
+						<span>
+							<p>Web Development Back-end</p>
+						</span>
+						<span>
+							<p>Contest Programming</p>
+						</span>
+						<span>
+							<p>Hardware Project</p>
+						</span>	
+						
+					</div>
+
+			</div>
+			
+				<!-- experience stop -->
+
+				<!-- skills -->
+			<div class="skills" id="skills">
+
+			<h2>Front End</h2>
+				<div class="front">
+					
+					<div class="frame">
+						<img src="img/html.png">
+						<p>HTML</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/css.png">
+						<p>CSS</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/sass.png">
+						<p>SASS</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/boot.png">
+						<p>BOOTSTRAP</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/js.png">
+						<p>Javascript</p>
+					</div>
+					
+				</div>
+				<h2>Backend</h2>
+				<div class="back">
+					
+					<div class="frame">
+						<img src="img/php.png">
+						<p>PhP</p>
+					</div>
+					
+
+					<div class="frame">
+						<img src="img/mysql.png">
+						<p>mysql</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/jqury.png">
+						<p>jquery</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/sql.png">
+						<p>sql</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/php-ma.png">
+						<p>PhP myadmin</p>
+					</div>
+					
+				</div>
+				<h2>Tools work with</h2>
+				<div class="tools">
+					
+					<div class="frame">
+						<img src="img/sublime.png">
+						<p>Sublime Text</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/atom.png">
+						<p>Atom</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/vs.png">
+						<p>Visual Studio</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/codepen.png">
+						<p>Codepen</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/mysql-wb.png">
+						<p>Mysql Workbench</p>
+					</div>
+					
+				</div>
+				<h2>Others</h2>
+				<div class="others">
+					
+					<div class="frame">
+						<img src="img/wp.png">
+						<p>Wordpress</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/ps.png">
+						<p>Photoshop</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/pspp.png">
+						<p>Premiere Pro</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/ai.png">
+						<p>Illustrator</p>
+					</div>
+					
+					<div class="frame">
+						<img src="img/cpp.png">
+						<p>C/C++</p>
+					</div>
+					
+					<!-- endl of skills -->
+				</div>
+								
+			</div>
+			
+			<div class="works" id="works">
+
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel" data-slide-to="1"></li>
+							<li data-target="#myCarousel" data-slide-to="2"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner">
+							<div class="item active">
+								<img src="img/1.jpg" alt="Los Angeles" style="width:100%; height: 600px;">
+							</div>
+
+							<div class="item">
+								<img src="img/2.jpg" alt="Chicago" style="width:100%; height: 600px;">
+							</div>
+							
+							<div class="item">
+								<img src="img/3.jpg" alt="New york" style="width:100%; height: 600px;">
+							</div>
+							</div>
+
+							<!-- Left and right controls -->
+							<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+							<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+							<span class="sr-only">Next</span>
+							</a>
+						</div>
+
+			</div>
+				
+			<div class="last-content" id="last-content">
+
+					<div class="training">
+						<h2>Training</h2>
+						<p>
+							Top-Up IT Training of ICT Division Bangladesh
+						</p>
+						<p>
+							NASSCOM IT-ITES Sector Skill Council (SSC)
+						</p>
+						<p>
+							Certified by <br> George Washington University, USA.
+						</p>
+						
+					</div>
+					<div>
+						
+					</div>
+
+					<div class="education"> 
+						<h2>Education</h2>
+						<p>
+							Institute of Science & Technology
+						</p>
+						<p>
+							National University of Bangladesh
+						</p>
+						<p>
+							Electronincs and Communication Engineering (ECE)
+						</p>
+
+					</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
---
+++ sidebar/sidebar.php
@@ -0,0 +1,19 @@
+<style>
+<?php include"sidebar.css";?>
+</style>
+
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>Sidebar</h2>
+            <ul>
+            <li><a href="../index.php"><i class="fas fa-home"></i>Home</a></li>
+            <li><a href="#profile"><i class="fas fa-user"></i>Profile</a></li>
+            <li><a href="#about"><i class="fas fa-address-card"></i>About</a></li>
+            <li><a href="#experience"><i class="fas fa-project-diagram"></i>Portfolio</a></li>
+            <li><a href="#skills"><i class="fas fa-blog"></i>Blogs</a></li>
+            <li><a href="#works"><i class="fas fa-address-book"></i>Contact</a></li>
+            <li><a href="#last-ceontent"><i class="fas fa-map-pin"></i>Map</a></li>
+            </ul>
+        </div>
+    </div>
+
